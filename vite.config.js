24782f09ec9cd02f21d6ff94a8f9272009e9c6cc
Brendan Munnelly <munnellyb@gmail.com>
2024-03-12 06:39:33 +0000

    some fixes

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/app-react-basic1/"
+  base: "/app-react-basic1"
 })
 
