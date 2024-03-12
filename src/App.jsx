0d27ad929b21fef970e299aebccdd10d628b19c5
Brendan Munnelly <munnellyb@gmail.com>
2024-03-12 03:31:17 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import Header from './Header';
+import MainContent from './MainContent';
+import Footer from './Footer';
+import './Petite.css'
+	
+function App() {
+   return (
+   <>
+      <Header />
+      <MainContent 
+          companyName = "ABC Corporation" 
+          str = "Hello"
+          num = {42}
+          truth = {true}
+          arr = {[99]}
+          obj = {{name: 'Alice'}}         
+      />
+      <Footer publishMonth = "October" publishYear = "2022" />
+   </>
+   );
+}
+	
+export default App;
---
+++ src/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+    return (
+      <header>
+         <h1>Corporate strategy</h1>
+      </header>  
+    )
+ }
+     
+ export default Header;
